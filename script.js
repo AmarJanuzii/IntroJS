d999c9945b510a7e0f26e2e1a81f79c8649367f8
AmarJanuzii <78106627+AmarJanuzii@users.noreply.github.com>
2022-11-10 18:57:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// let numberOne = 100;
+
+// let numberTwo = 20;
+
+// let sum ;
+
+// sum = numberOne + numberTwo;
+
+// console.log(sum)
+
+// const schoolName = "Innovation Academy";
+
+// let nameSurname = "Amar Januzi";
+// let schoolName = "Innovation Academy";
+// let age = 13;
+// let city = "Vushtrri";
+// let isStudent = true;
+
+// let months = ["October", "November", "December", "January"];
+
+// document.write(months);
+
+// let info = ["Amar Januzi", "Innovation Academy", 13, "Vushtrri", true ];
+
+// document.write(info)
+
+
+
+
+
+
+
+
+// document.write("Name:" + nameSurname + "<br>");
+// document.write("School:" + schoolName + "<br>");
+// document.write("Age:" + age + "<br>");
+// document.write("City:" + city + "<br>");
+// document.write("Is Student:" + isStudent + "<br>");
+
+// let newText ='"I am the best" dev here';
+
+// let trueFalse = "False";
+
+// console.log(numberOne);
+
+// console.log(newText);
+
+// console.log(trueFalse);
+ 
